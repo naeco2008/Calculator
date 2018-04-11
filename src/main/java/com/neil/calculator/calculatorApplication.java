638c28c2377893bf5be73a837dcba05d9bd8d74a
naeco2008 <naeco72@hotmail.com>
2018-04-11 10:31:12 +0800

    intial commit

---
+++ src/main/java/com/neil/calculator/calculatorApplication.java
@@ -0,0 +1,16 @@
+package com.neil.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class calculatorApplication {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(calculatorApplication.class, args);
+	}
+}
